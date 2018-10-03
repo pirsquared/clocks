46bf3e13be27b84dfecb4cca5413f366e6c53200
Sean Smith <1900410+pirsquared@users.noreply.github.com>
2018-10-03 15:45:29 -0700

    basics are done

--- TxtClock/clock.js
+++ TxtClock/clock.js
@@ -1,83 +1,80 @@
 document.addEventListener('DOMContentLoaded', domloaded, false);
 
 function domloaded() {
+    const numbers = [
+    	[1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1], // 0
+    	[1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1], // 1
+    	[1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1], // 2
+    	[1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1], // 3
+    	[1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1], // 4
+    	[1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1], // 5
+    	[1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1], // 6
+    	[1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0], // 7
+    	[1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1], // 8
+    	[1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1]  // 9
+    ];
     let canvas = document.getElementById('canvas');
     let ctx = canvas.getContext('2d');
-
-    let o = {
-        c: {s: '#200', m: '#020', h: '#002', t: 'black'},
-        r: {s: 75, m: 125, h: 175},
-        x: 250, y: 250, w: 40, cap: 'butt', blur: 15,
-    };
-
-    function degToRad(degree) {
-        let factor = Math.PI / 180;
-        return degree * factor
-    }
 
     function pad(num, size) {
         let s = "00" + num;
         return s.substr(s.length-size);
     }
 
-    function arc(cx, cy, r, b, e, fill) {
-        ctx.strokeStyle = fill;
-        ctx.beginPath();
-        ctx.arc(cx, cy, r, degToRad(b), degToRad(e));
-        ctx.stroke();
-        ctx.closePath();
-    }
-
-    function hex(s, m, h) {
-        let s_ = pad((Math.floor(s) + 196).toString(16), 2);
-        let m_ = pad((Math.floor(m) + 196).toString(16), 2);
-        let h_ = pad((Math.floor(h % 12 * 5) + 196).toString(16), 2);
-        return "#" + s_ + m_ + h_;
-    }
-
     function renderTime() {
-        let now          = new Date();
-        let today        = now.toDateString();
-        let hours        = now.getHours();
-        let minutes      = now.getMinutes();
-        let seconds      = now.getSeconds();
-        let milliseconds = now.getMilliseconds();
-        let seconds_     = seconds + milliseconds / 1000;
-        let minutes_     = minutes + seconds_ / 60;
-        let hours_       = hours + minutes_ / 60;
+        let now     = new Date();
+        let today   = now.toDateString();
+        let hours   = now.getHours();
+        let minutes = now.getMinutes();
+        let seconds = now.getSeconds();
+        let digits  = [Math.floor(hours / 10), hours % 10, Math.floor(minutes / 10), minutes % 10]
 
-        let shadowColor  = hex(seconds_, minutes_, hours_);
+        // stuff
+        let w = 25;
+        let h = 20;
+        let W = 500;
+        let mid_x = Math.floor(W / 2);
+        let H = 500;
+        let mid_y = Math.floor(H / 2);
 
         // Background
         ctx.fillStyle = 'black';
         ctx.fillRect(0, 0, 500, 500);
 
-        // Time
-        // ctx.font = 'bold 120px Courier';
-        // ctx.fillText(time, 30, 230);
-        // ctx.font = 'bold 50px Courier';
-        // ctx.fillText(ampm, 400, 185);
-        //
-        // Date
-        // ctx.font = 'bold 46px Courier New';
-        // ctx.fillText(today, 46, 290);
+        ctx.strokeStyle = '#ff0';
+        ctx.lineWidth = 5;
+        ctx.beginPath();
+        ctx.moveTo(mid_x, .0 * H);
+        ctx.lineTo(mid_x, .2 * H);
+        ctx.stroke();
 
-        ctx.font = 'bold 12px Courier';
-        ctx.fillStyle = 'white';
-        for (let i = 0; i < 60; i++) {
-            let k = Math.floor(i / 15);
-            let j = i % 15;
-            let r = Math.floor(j / 3);
-            let c = j % 3;
+        for (let i0 = 0; i0 < 60; i0++) {
+            ctx.font = 'bold 18px Courier';
+            ctx.fillStyle = '#333';
+            let g0 = Math.floor(i0 / 30);
+            let i1 = i0 % 30;
+            let g1 = Math.floor(i1 / 15);
+            let i2 = i1 % 15;
+            let r = Math.floor(i2 / 3);
+            let c = i2 % 3;
+
+            let dig = g1 + g0 * 2;
+            let sign = g0 === 0 ? -1 : 1;
+            let acol = dig * 3 + c ;
+            let amul = Math.abs(acol - 5.5) + .5 * (g0 === g1) + 1;
+
+            ctx.fillStyle = i0 === seconds ? '#0f0' : !numbers[digits[dig]][c * 5 + r] ? '#333' : '#fff';
+            ctx.textAlign = g0 === 0       ? 'left' : 'right';
+
             ctx.fillText(
-                pad(i, 2),
-                50 + k * 100 + c * 25,
-                50 + r * 25
+                pad(i0, 2),
+                mid_x + amul * sign * w,
+                (r + 1) * h,
             );
         }
 
     }
 
-    renderTime()
-    // setInterval(renderTime, 40)
+    // renderTime()
+    setInterval(renderTime, 40)
 }
